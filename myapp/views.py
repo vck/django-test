86b23180a11df7d1a4e7028de6810451928ebcec
vck <vicky.vernando@student.unri.ac.id>
2019-10-13 18:53:55 +0700

    add Procfile

--- myapp/views.py
+++ myapp/views.py
@@ -3,20 +3,27 @@ import json
 import pandas as pd 
 import sys
 
-filename = sys.path[0] + "/myapp/data/data.csv"
+FILENAME = sys.path[0] + "/myapp/data/data.csv"
 
 def index_view(request):
 	data = "hello world!"
 	return HttpResponse(json.dumps(data))
 
 def endpoint(request):
-	df 		= pd.read_csv(filename)
-	targets	= ["PM2.5", "PM10", "SO2", "NO2", "CO", "O3"]
+	df 		= pd.read_csv(FILENAME)
+	targets	= ["PM2.5", 
+				"PM10", 
+				"SO2", 
+				"NO2", 
+				"CO", 
+				"O3"]
 	data = dict()
 	bellow = dict()
+
 	for target in targets:
 		bellow[target] = df[df[target] < 20][target].to_numpy().std()
 	data["bellow"] = bellow
+	
 	above = dict()
 	for target in targets:
 		above[target] = df[df[target] > 20][target].to_numpy().std()
