f53c3eaf988f98f228777d99fc96aa202774c14b
vck <vicky.vernando@student.unri.ac.id>
2019-10-14 22:34:57 +0700

    simplified stuff

--- myapp/views.py
+++ myapp/views.py
@@ -8,7 +8,7 @@ ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 FILENAME = ROOT_DIR+"/staticfiles/data.csv"
 
 def index_view(request):
-	data = "hello world!"
+	data = dict(msg="Hello!", plain="Antler", md5="fc4acdfe839b3b71bbc4d702ff3220")
 	return HttpResponse(json.dumps(data))
 
 def endpoint(request):
@@ -16,18 +16,18 @@ def endpoint(request):
 	targets	= ["PM2.5", 
 				"PM10", 
 				"SO2", 
-				"NO2", 
-				"CO", 
-				"O3"]
-	data = dict()
-	bellow = dict()
+               	"NO2", 
+               	"CO",
+               	"O3"]
+
+	data 	= dict()
+	bellow	= dict()
+	above	= dict()
 
 	for target in targets:
-		bellow[target] = df[df[target] < 20][target].to_numpy().std()
-	data["bellow"] = bellow
+		bellow[target]	= df[df[target] < 20][target].to_numpy().std()
+		above[target]	= df[df[target] > 20][target].to_numpy().std()
 	
-	above = dict()
-	for target in targets:
-		above[target] = df[df[target] > 20][target].to_numpy().std()
-	data["above"] = above
+	data["bellow"]	= bellow
+	data["above"]	= above
 	return HttpResponse(json.dumps(data))
